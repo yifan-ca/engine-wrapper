adbd9ea257eac6cf63d183f1afb7b52b3e50455d
Cristina Silva <crissilva.eng@gmail.com>
2021-06-13 06:34:23 -0300

    feat: adds base healthcheck to memory and disk

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnginesModule } from './engines/engines.module';
+import { HealthModule } from './health/health.module';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [EnginesModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
